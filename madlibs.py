f98b108a925b7a31a3f6272bd1b947111f656a50
Jacob <jelondon12@gmail.com>
2021-03-12 17:51:32 -0800

    Improved printing

--- madlibs.py
+++ madlibs.py
@@ -14,17 +14,17 @@ NORTHSOUTH = ['north', 'south']
 EASTWEST   = ['east', 'west']
 MAGICTYPE  = ['Abjuration', 'Conjuration', 'Divination', 'Enchantment', 'Evocation', 'Illusion', 'Necromancy', 'Transmutation']
 ELEMENT    = ['Air', 'Earth', 'Fire', 'Water', 'Void', 'Ice']
-FIGHT      = ['alongside the people', 'alongside the Elite', 'using distance']
-PARTY      = ['Loyalist', 'Separatists']
-BATTLE     = ['present, and I #VERBED',
-              'not present and I #VERBED. During the siege of Martnya I was #VERBED',
-              'not present and I #VERBED. During the battle of Pendremic I #VERBED']
-SINCE      = ['my troops',
-              'my martial skill',
-              'culture and Technology',
-              'the economy',
-              'politics',
-              'magic abilities and religion']
+PARTY      = [
+    "federalist", "republican", "liberal", "libertarian", "democrat", "separtist",
+    "communist", "imperialist", "xenophobe", "republic",
+]
+BODYPART   = [
+    "privates", "arm", "leg", "head", "toe", "hand", "eyeball", "nose", "earlobe",
+    "mouse", "nostril", "humorous", "femur", "ankle", "skull", "gallbladder", "liver",
+    "tearduct", "elbow", "bicep", "epidermis", "tricep", "quadricep", "gluteus maximum",
+    "chest", "stomache", "belly", "bellybutton", "tongue", "teeth", "fingernail",
+    "eyelash", "freckle", "cuticle", "clavicle", "mouth", "teeth", "ear",
+]
 
 with open(FILE_NAMES,      'r') as f: names      = f.read().split()
 with open(FILE_PLACES,     'r') as f: places     = f.read().split()
@@ -32,52 +32,60 @@ with open(FILE_NOUNS,      'r') as f: nouns      = f.read().split()
 with open(FILE_VERBS,      'r') as f: verbs      = f.read().split()
 with open(FILE_ADJECTIVES, 'r') as f: adjectives = f.read().split()
 with open(FILE_ADVERBS,    'r') as f: adverbs    = f.read().split()
+
 def get(thelist):
     return random.choice(thelist)
+
+def get_number():
+    if random.randint(0, 1) == 0:
+        return str(random.uniform(-1e10, 1e10))
+    return str(random.randint(-1000000, 1000000))
 
 lookup = {
     "#RACE":       lambda: get(RACE),
     "#NAME":       lambda: get(names),
     "#PLACE":      lambda: get(places),
     "#NOUN":       lambda: get(nouns),
-    "#NOUNS":      lambda: get(nouns) + "s",
     "#ADJECTIVE":  lambda: get(adjectives),
     "#ADVERB":     lambda: get(adverbs),
     "#NORTHSOUTH": lambda: get(NORTHSOUTH),
     "#EASTWEST":   lambda: get(EASTWEST),
     "#MAGICTYPE":  lambda: get(MAGICTYPE),
     "#ELEMENT":    lambda: get(ELEMENT),
-    "#PARTY":      lambda: get(PARTY),
-    "#FIGHT":      lambda: get(FIGHT),
-    "#VERBING":    lambda: get(verbs) + 'ing',
     "#VERB":       lambda: get(verbs),
-    "#VERBED":     lambda: get(verbs) + 'ed',
-    "#BATTLE":     lambda: get(BATTLE),
-    "#SINCE":      lambda: get(SINCE),
+    '#NUMBER':     lambda: get_number(),
+    '#BODYPART':   lambda: get(BODYPART),
 }
 
-def conv_split(words, show=False):
-    for i, word in enumerate(words):
-        cleaned =  '#' + re.sub(r'\W+', '', word)
-        if cleaned in lookup.keys():
-            words[i] = word.replace(cleaned, lookup[cleaned]())
-        if '.' in words[i] or '?' in words[i]:
-            words[i] += '\n'
-
-    if show:
-        print(" ".join(words))
-    else:
-        with open('out.txt', 'w') as f:
-            f.write(" ".join(words))
+def conv_chars(string):
+    begin = 0
+    i = 0
+    accumulate = ""
+    while i < len(string):
+        if string[i] == '#':
+            yield string[begin:i]
+            accumulate += '#'
+            i += 1
+            continue
+        
+        if accumulate:
+            accumulate += string[i]
+            if accumulate in lookup.keys():
+                yield lookup[accumulate]()
+                accumulate = ""
+                begin = i + 1
+        i += 1
+    yield string[begin:]
 
 def conv_file(filename):
-    words = []
     with open(filename, 'r') as f:
-        words = f.read().split()
-    conv_split(words)
+        text = f.read()
+    for subtext in conv_chars(text):
+        print(subtext, end='')
+    print()
 
 def conv(text):
-    conv_split(text.split(), show=True)
+    conv_chars(text)
 
 if len(argv) != 3:
     print("""
